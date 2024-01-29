6f0c6a5196b242379c9dfe8fd34cca2b5da7128a
rosg150498 <rosg150498@gmail.com>
2024-01-29 20:35:50 +0000

    Add host name to ALLOWED_HOSTS to settings.py to thrive sync folder to file directory

--- thrivesync/settings.py
+++ thrivesync/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!sw%@t%39$ec$i^(2zm2!5*d4uip9#_#+g$4ntsdxxdni%14t-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-rosg150498-thrivesync-fqp8dhm21e3.ws-eu107.gitpod.io']
 
 
 # Application definition
