6f085f342c59d20f48c0a0d786b03f6e2b1039d5
rosg150498 <rosg150498@gmail.com>
2024-01-29 21:53:26 +0000

    Move index.html to project folder to file directory

--- thrivesync/urls.py
+++ thrivesync/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import index
+from project.views import index
 
 urlpatterns = [
     path('', index, name='index'),
