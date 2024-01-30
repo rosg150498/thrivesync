24f06361103bea6ef77751d6713e0aa5e116975e
rosg150498 <rosg150498@gmail.com>
2024-01-30 20:39:52 +0000

    Update folder and file structure to projectapp and app to file directory

--- app/settings.py
+++ app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import OS
 
 from pathlib import Path
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'thrivesync.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'project/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
