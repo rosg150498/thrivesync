5ad31b7667251274dc3946e44a0d272611829832
rosg150498 <rosg150498@gmail.com>
2024-02-01 20:35:02 +0000

    Connect to database URL to settings.py

--- thrivesync/settings.py
+++ thrivesync/settings.py
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'thrivesync.wsgi.application'
 # }
 
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
