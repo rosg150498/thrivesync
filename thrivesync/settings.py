488dc5d1df368a0a6bd138fecc812898cee7e1a0
rosg150498 <rosg150498@gmail.com>
2024-02-01 22:55:05 +0000

    Configure PostgreSQL database to settings.py

--- thrivesync/settings.py
+++ thrivesync/settings.py
@@ -17,6 +17,11 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+
+DATABASE_URL = 'postgres://dbmqivcy:TVmmCCMz0sDqKY8b3Gm-59ExL8L9QiEY@mel.db.elephantsql.com/dbmqivcy'
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
