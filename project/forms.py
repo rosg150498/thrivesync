ee86a0ec4704c3a7840012de463c978aedd52609
rosg150498 <rosg150498@gmail.com>
2024-02-03 15:47:58 +0000

    Add custom user creation form to forms.py

--- project/forms.py
+++ project/forms.py
@@ -1,3 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
+
+    forename = forms.CharField(max_length=30)
+    surname = forms.CharField(max_length=30)
+    gender = forms.ChoiceField(choices=GENDER_CHOICES)
+    email = forms.EmailField(max_length=254)
+    phone_number = forms.CharField(max_length=15)
+    username = forms.CharField(max_length=30)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('forename', 'surname', 'gender', 'email', 'phone_number', 'username', 'password1', 'password2')
