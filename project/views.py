6dc8662d7fab37e699709ab0232f4237254afc2d
rosg150498 <rosg150498@gmail.com>
2024-02-04 19:22:09 +0000

    Add user registration view function to views.py

--- project/views.py
+++ project/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from project.forms import CustomUserCreationForm
 
 # Create your views here.
 def index(request):
