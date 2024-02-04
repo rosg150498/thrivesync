3a84c6a448f8d83d041dee866564fc78932da7a5
rosg150498 <rosg150498@gmail.com>
2024-02-04 01:15:32 +0000

    Add login path to urls.py

--- thrivesync/urls.py
+++ thrivesync/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('register/', register, name='register'),
+    path('login/', login, name='login'),
 ]
