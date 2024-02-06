370d9863e7dd5a7385a4b4b2ec0085e8448df211
rosg150498 <rosg150498@gmail.com>
2024-02-06 23:47:04 +0000

    Update customer user creation form and custom authentication forms to forms.py

--- project/forms.py
+++ project/forms.py
@@ -1,7 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
 
 class CustomUserCreationForm(UserCreationForm):
     GENDER_CHOICES = (
@@ -16,26 +15,26 @@ class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(max_length=254)
     phone_number = forms.CharField(max_length=15)
     username = forms.CharField(max_length=30)
-    password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ('forename', 'surname', 'gender', 'email', 'phone_number', 'username', 'password1', 'password2')
 
-
-class CustomUserCreationForm(UserCreationForm):
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
-
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
-
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError('Passwords do not match.')
-
+            raise forms.ValidationError("Passwords don't match")
         return password2
 
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
